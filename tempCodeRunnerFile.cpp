d88a258804a61108eaee1af638e4a81027cde851
SaranshGupta123 <73605084+SaranshGupta123@users.noreply.github.com>
2022-06-23 18:50:36 +0530

    added one error and some code file and some exe file

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+void performOperationsScientific()
+    // {
+    //     cout << "The value of cos(a) is: " << cos(a) << endl;
+    //     cout << "The value of sin(a) is: " << sin(a) << endl;
+    //     cout << "The value of exp(a) is: " << exp(a) << endl;
+    //     cout << "The value of tan(a) is: " << tan(a) << endl;
+    // }
